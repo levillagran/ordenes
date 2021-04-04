a8f6370dee7b4fd76b073ab8a95308fccf53422c
LENIN <LENIN@Old.mshome.net>
2021-04-04 17:55:10 -0500

    creación del proyecto

---
+++ back-ordenes/src/main/java/com/altiora/BackOrdenesApplication.java
@@ -0,0 +1,13 @@
+package com.altiora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOrdenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOrdenesApplication.class, args);
+	}
+
+}
---
+++ back-ordenes/src/test/java/com/altiora/BackOrdenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altiora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOrdenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
